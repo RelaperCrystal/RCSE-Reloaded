47daa01357c4914cc5d10af12d3f80ead97a214a
RelaperCrystal <37800788+RelaperCrystal@users.noreply.github.com>
2019-09-25 20:59:42 +0800

    Include Microsoft.WindowsAPICodePack-Shell by @Aybe

---
+++ RCSE-Reloaded.API/ErrorForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RCSE_Reloaded.API
+{
+    partial class ErrorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelSubTitle = new System.Windows.Forms.Label();
+            this.labelActions = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 52);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelTitle.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelTitle.Location = new System.Drawing.Point(77, 13);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(251, 21);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "非常抱歉，但 RCSE 引发了错误。";
+            // 
+            // labelSubTitle
+            // 
+            this.labelSubTitle.AutoSize = true;
+            this.labelSubTitle.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelSubTitle.Location = new System.Drawing.Point(78, 36);
+            this.labelSubTitle.Name = "labelSubTitle";
+            this.labelSubTitle.Size = new System.Drawing.Size(289, 17);
+            this.labelSubTitle.TabIndex = 2;
+            this.labelSubTitle.Text = "您可以考虑在 Github Repo 上的 Issues 进行反馈。";
+            // 
+            // labelActions
+            // 
+            this.labelActions.AutoSize = true;
+            this.labelActions.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelActions.Location = new System.Drawing.Point(13, 67);
+            this.labelActions.Name = "labelActions";
+            this.labelActions.Size = new System.Drawing.Size(176, 17);
+            this.labelActions.TabIndex = 3;
+            this.labelActions.Text = "您可以考虑在下列选项中选择：";
+            // 
+            // ErrorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 353);
+            this.Controls.Add(this.labelActions);
+            this.Controls.Add(this.labelSubTitle);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Symbol", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ErrorForm";
+            this.Text = "错误";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelSubTitle;
+        private System.Windows.Forms.Label labelActions;
+    }
+}
---
+++ RCSE-Reloaded.API/HtmlViewerForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RCSE_Reloaded.API
+{
+    public partial class HtmlViewerForm : Form
+    {
+        public HtmlViewerForm(string url)
+        {
+            InitializeComponent();
+            try
+            {
+                this.webBrowser1.Url = new Uri(url);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("无法进行预览，因为 URL 无效。\r\n如果您是通过程序内部进入本窗口，您可以考虑发一个\r\nIssue 到 Github 上。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void HtmlViewerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
