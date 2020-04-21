54d28092bd5ffbed75716588642e9a90a67845cf
rcdraft <19248934+rcdraft@users.noreply.github.com>
2020-04-21 12:52:36 +0800

    重做错误窗口，更换关于窗口图片

--- CommonVals.cs
+++ CommonVals.cs
@@ -9,8 +9,8 @@ namespace RCSE_Reloaded
     class CommonVals
     {
         public static string filters = "C# 源代码 (*.cs)|*.cs|C++ 源代码 (*.cpp;*.cxx)|*.cpp;*.cxx|Visual Basic 源代码 (*.vb)|*.vb|HTML (*.htm;*.html)|*.htm;*.html|XAML (*.xml)|*.xml|批处理文件 (*.bat;*.cmd)|*.bat;*.cmd|JAVA (*.java)|*.java|Python (*.py)|*.py|所有文件 (*.*)|*.*";
-        public const string snapshot = "21:20-15-A_S";
-        public const string verNumber = "1.0.1";
+        public const string snapshot = "21:20-17-A";
+        public const string verNumber = "1.1";
         public const bool isSnapshot = true;
         public const string programName = "SimpleEditor";
         public const string programFullName = "RelaperCrystal's Simple Editor";
